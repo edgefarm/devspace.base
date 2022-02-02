cfd5281e406235a7638886e10b37cc59304b14e3
Florian Reinhold <florian.reinhold@ci4rail.com>
2022-02-02 21:17:44 +0100

    feat: added mkcert environment

---
+++ environments/mkcert/hack/purge-ca.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    echo "purge-ca <ca_name>"
+    exit
+fi
+
+if [ -d "${CA_DIR}/$1" ]; then
+    echo "Deleting CA $1"
+    rm -rf ${CA_DIR}/$1
+fi
